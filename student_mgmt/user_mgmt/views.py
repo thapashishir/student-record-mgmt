e868af212a2237302af82474751c407cde5f33f4
shishir-uxl <shishirthapa521@gmail.com>
2022-07-29 17:46:31 +0545

    gitignore file updated

--- student_mgmt/user_mgmt/views.py
+++ student_mgmt/user_mgmt/views.py
@@ -1,6 +1,9 @@
+import hashlib
+
 from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.http import HttpResponse
+from . models import User
 
 # Create your views here.
 class UserLoginView(View):
@@ -8,35 +11,44 @@ class UserLoginView(View):
         return render(request, "user_mgmt/login.html")
 
     def post(self,request):
-        users = [
-            {
-                "user_name" : "admin",
-                "password"  : "admin",
-                "role"      : "admin"
-            },
-            {
-                "user_name" : "user",
-                "password"  : "user",
-                "role"      : "user"
+        #calculating hash of password
+        password_hash = hashlib.sha256(request.POST.get("password").encode('utf-8')).hexdigest()
+        user_name = request.POST.get("user_name")
+        user = User.objects.filter(user_name = user_name, password = password_hash).first()
+       
+        #if only there is user data rows database
+        if  user: 
+
+            #setting data to django session named as current_user
+            request.session["current_user"] = {
+                "user_name":  user.user_name,
+                "role": "admin"
             }
 
-        ]
-        user_name = request.POST.get("user_name")
-        password = request.POST.get("password")
-
-        for user in users:
-
-            if  user_name == user["user_name"] and password == user["password"]: 
-                request.session["current_user"] = {
-                    "user_name":  user["user_name"],
-                    "role": user["role"]
-                }
-                return redirect("/student/list")
-        
+            #redirect to student list if user name passoword is valid
+            return redirect("/student/list")
+ 
         return  HttpResponse("<h1>Invalid User Name Password</h1>")
 
 class UserLoginOutView(View):
     def get(self, request):
         request.session["current_user"] = None
         return redirect("/login")
+
+class UserAddView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "user_mgmt/user_add.html")
+
+    def post(self, request, *args, **kwargs):
+        #calculating hash of password
+        password_hash = hashlib.sha256(request.POST.get("password").encode('utf-8')).hexdigest()
+        data = {
+            "user_name": request.POST.get("user_name"),
+            "password": password_hash,
+            "role": request.POST.get("role")
+        } 
+        user = User.objects.create(**data)
+        user.save()
+        return redirect('/login')
+
 
